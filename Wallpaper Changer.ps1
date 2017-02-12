7fa36757de4bb5a9ec2ae7e5eb4878bb28b0c32b
VictorShepelin <victor.v.shepelin@live.ru>
2017-02-12 14:37:59 +0300

    Initial commit

---
+++ Wallpaper Changer.ps1
@@ -0,0 +1,69 @@
+﻿# Wallpaper Changer version 1.0 - release date: 11.02.2017
+
+$WallpaperType = add-type @"
+using System;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+namespace Wallpaper {
+    public enum Style : int  {
+        Tile, Center, Stretch, NoChange, Fit
+    }
+    public class Setter {
+        public const int SetDesktopWallpaper = 20;
+        public const int UpdateIniFile = 0x01;
+        public const int SendWinIniChange = 0x02;
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        private static extern int SystemParametersInfo (int uAction, int uParam, string lpvParam, int fuWinIni);
+        public static void SetWallpaper ( string path, Wallpaper.Style style ) {
+            SystemParametersInfo( SetDesktopWallpaper, 0, path, UpdateIniFile | SendWinIniChange );
+            RegistryKey key = Registry.CurrentUser.OpenSubKey("Control Panel\\Desktop", true);
+            switch( style ) {
+                case Style.Stretch :
+                key.SetValue(@"WallpaperStyle", "2");
+                key.SetValue(@"TileWallpaper", "0");
+                break;
+                case Style.Center :
+                key.SetValue(@"WallpaperStyle", "1");
+                key.SetValue(@"TileWallpaper", "0");
+                break;
+                case Style.Tile :
+                key.SetValue(@"WallpaperStyle", "1");
+                key.SetValue(@"TileWallpaper", "1");
+                break;
+                case Style.Fit :
+                key.SetValue(@"WallpaperStyle", "6");
+                key.SetValue(@"TileWallpaper", "0");
+                break;
+                case Style.NoChange :
+                break;
+            }
+            key.Close();
+        }
+    }
+}
+"@ -Passthru
+$Path = "D:\OneDrive\Pictures\Girls\(Laba-ws.blogspot.com)_140_Bikini_ 0061.jpg"
+[Wallpaper.Setter]::SetWallpaper((Convert-Path $Path), "Fit")
+
+$Pictures = "D:\OneDrive\Pictures" #каталог картинок
+$Girls = "Girls" #папка с девчонками
+$WishList = "Celebrity", "Drive", "Graphics", "Nature", "Others", "Space", "Enclosure" #предпочитаемые папки
+$BlackList = "Camera Roll", "LifeCam Files", "Some", "Twonky", "С проигрывателя Victor", "С телефона Windows Phone Lena", "From Victor", "Raptr Screenshots", "toSort", "Из Submarine", "Файлы LifeCam", "Saved Pictures", "Instagram", "Private", "Screenshots", "Пленка" #исключаемые папки
+if (Get-Item "D:\OneDrive\My Scripts\Wallpapers\addnude.flag") {
+    $ExcludeList = $BlackList
+    if (Get-Item "D:\OneDrive\My Scripts\Wallpapers\girlsonly.flag") {
+        $ExcludeList = $BlackList + $WishList
+        }
+    }
+else {
+    $ExcludeList = $BlackList + $Girls
+    }
+$file = (Get-ChildItem $Pictures -Exclude $ExcludeList).GetFiles() | Get-Random
+[Wallpaper.Setter]::SetWallpaper($file.FullName, "Fit")
+
+<# History
+ based on http://poshcode.org/2603
+ version 1.0 - release date: 11.02.2017
+    add - first release
+ Email for support to Victor.V.Shepelin@live.ru
+ #>
