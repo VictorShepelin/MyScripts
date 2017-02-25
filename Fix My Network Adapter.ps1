eb17eab9edb4c9bc41c5489272e31cb3b7f832e2
VictorShepelin <victor.v.shepelin@live.ru>
2017-02-25 13:39:23 +0300

    set

--- Fix My Network Adapter.ps1
+++ Fix My Network Adapter.ps1
@@ -1,4 +1,2 @@
 Get-NetAdapter -Name "Ethernet" | Set-NetAdapterBinding -ComponentID "ms_tcpip" -Enabled $true
 Get-NetAdapter -Name "Ethernet" | Set-NetAdapterBinding -ComponentID "ms_lltdio" -Enabled $true
-Get-NetAdapter -Name "Ethernet" | Set-NetAdapterBinding -ComponentID "ms_tcpip" -Enabled $false
-Get-NetAdapter -Name "Ethernet" | Set-NetAdapterBinding -ComponentID "ms_lltdio" -Enabled $false
