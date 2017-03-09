33c496c5a6fb3dc1e23bb74fc10692bd9f168231
VictorShepelin <victor.v.shepelin@live.ru>
2017-03-09 15:22:05 +0300

    set

---
+++ MoviesDB.ps1
@@ -0,0 +1,30 @@
+$strPath = "D:\OneDrive\Desktop\MoviesDB.xlsx"
+$Folder = "C:\Videos"
+$items = Get-ChildItem -Recurse -Path $Folder
+[System.Threading.Thread]::CurrentThread.CurrentCulture = New-Object "System.Globalization.CultureInfo" "en-US"
+$excel = New-Object -ComObject Excel.Application
+$excel.Visible = $true
+$workbook = $excel.Workbooks.Add()
+$workbook.Worksheets.Item(1).name = "Movies"
+$sheet = $workbook.Worksheets.Item("Movies")
+$sheet.cells.item(1,1) = "File Path"
+$sheet.cells.item(1,2) = "IMDB Url"
+$x = 2 # счетчик со второй строки т.к. первая это шапка
+$j = 0
+Do {
+    $sheet.cells.item($x, 1) = $items[$j].FullName
+    $x++
+    $j++
+    }
+While ($j -lt $items.count)
+#выравнивание
+$range = $sheet.usedRange
+$range.EntireColumn.AutoFit() | out-null
+#сохранение
+if (Test-Path $strPath) { 
+    Remove-Item $strPath
+    $Excel.ActiveWorkbook.SaveAs($strPath)
+    }
+else {
+    $Excel.ActiveWorkbook.SaveAs($strPath)
+    }
